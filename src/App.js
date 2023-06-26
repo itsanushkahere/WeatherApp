fd4196f21a9869ab59d5e39411a621be0d97e73d
Anushka Nitin Mahadik <85669264+itsanushkahere@users.noreply.github.com>
2023-06-26 16:45:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/weather/style.css';
+import Temp from './components/weather/temp';
+
+function App() {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+}
+
+export default App;
